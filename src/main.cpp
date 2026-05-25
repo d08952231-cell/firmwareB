87b2e99082a55244863c5dccd83945c9f154f506
CrabHub Official <d08952231@gmail.com>
2026-05-25 18:15:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,14 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include "LilyGo_T-Embed.h" // ОФИЦИАЛЬНАЯ БИБЛИОТЕКА
 
-// === ЖЕСТКО ЗАДАЕМ НАСТРОЙКИ ЭКРАНА ===
-#define USER_SETUP_INFO "LilyGO_TEmbed"
-#define ST7789_DRIVER
-#define TFT_WIDTH  135
-#define TFT_HEIGHT 240
-#define TFT_MOSI 23
-#define TFT_SCLK 18
-#define TFT_CS   5
-#define TFT_DC   15
-#define TFT_RST  13
-#define TFT_BL   27
-#define LOAD_GLCD
-#define LOAD_FONT2
-#define SPI_FREQUENCY  20000000
-
-#include <TFT_eSPI.h>
-
-TFT_eSPI tft = TFT_eSPI();
-
-// Функция для принудительного включения подсветки через чип TCA9554
-void turnOnBacklight() {
-  Wire.begin(18, 17); // Пины I2C на T-Embed
-  Wire.beginTransmission(0x38); // Адрес чипа подсветки
-  Wire.write(0x01); // Отправляем команду включения
-  Wire.endTransmission();
-  pinMode(27, OUTPUT);
-  digitalWrite(27, HIGH); // Дублируем сигналом на пин
-}
+LilyGo_TEmbed tft;
 
 void setup() {
   Serial.begin(115200);
 
-  // ВКЛЮЧАЕМ ПОДСВЕТКУ ПЕРЕД ИНИЦИАЛИЗАЦИЕЙ ЭКРАНА
-  turnOnBacklight();
+  // Официальная команда включения экрана и подсветки
+  tft.begin();
 
-  tft.init();
-  tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
   
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
